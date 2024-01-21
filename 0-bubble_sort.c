c2ca61cb1ea88a421926bc9952461f00451f58da
amina <amina.1111@yahoo.fr>
2024-01-21 18:27:02 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
+void bubble_sort(int *array, size_t size)
+
+{
+    size_t i;
+    int tmp;
+
+    for (i = 0; i < size; i++)
+    {
+        while (array[i] > array[i + 1] && array != NULL)
+        {
+            tmp = array[i];
+            array[i] = array[i + 1];
+            tmp = array[i + 1];
+            
+            if (tmp != array[size] && tmp != array[0])
+            {
+                printf(" ,");
+                printf("%d", tmp);
+                printf("\n");
+            }
+        }
+
+        if (array == NULL)
+
+        {
+            break;
+        }
+    }
+}
